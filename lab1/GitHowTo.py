1f78fb9157d6d3f7b9f244d8dad6696b0fe456bb
Dilnaz <dilnazeshmuhambet@icloud.com>
2025-09-15 21:38:05 +0500

    update all labs

---
+++ lab1/GitHowTo.py
@@ -0,0 +1,22 @@
+#1
+# git config --global user.name "Dilnazz0"
+# git config --global user.email "dilnazeshmuhambet@icloud.com"
+
+# git config --global init.defaultBranch main
+# git config --global core.autocrlf true
+# git config --global core.safecrlf warn
+
+#2 
+# mkdir work #newfolderopen
+# cd work
+# ls work
+# touch hello.html
+# git init
+# git add . #действует на все lab1,2 и тд
+# git commit -m "update all labs"
+# git add lab1 #действует только на это
+# git commit -m "lab1" #коментарий
+# git push #отправка
+# git status #показывает что изменилось в проекте
+# git log #получение списка произведенных изменений
+# git restore #используйте команду restore, чтобы сбросить содержимое файла 
