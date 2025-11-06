cd106692efbb7ff96b987ccacb893356221c056e
Dilnaz <dilnazeshmuhambet@icloud.com>
2025-11-07 02:23:38 +0500

    Lab 8

--- lab8/Snake.py
+++ lab8/Snake.py
@@ -1,6 +1,5 @@
 import pygame
 import random
-
 pygame.init()
 
 width = 600
@@ -16,11 +15,13 @@ blue = (120, 190, 255)
 green1 = (150, 230, 150)
 green2 = (130, 210, 130)
 
-food_img = pygame.image.load("images/apple.png")
-food_img = pygame.transform.scale(food_img, (cell, cell))
+foodimg = pygame.image.load("images/apple.png")
+foodimg = pygame.transform.scale(foodimg, (cell, cell))
 
 snake = [pygame.Rect(width//2, height//2, cell, cell)]
 direction = (cell, 0)
+eatsound=pygame.mixer.Sound('images/EatSound.ogg')
+diesound=pygame.mixer.Sound('images/Diesound.ogg')
 
 score = 0
 level = 1
@@ -28,13 +29,13 @@ speed = 7
 
 font = pygame.font.Font('images/minecraft_0.ttf', 20)
 gameoverfont = pygame.font.Font('images/minecraft_0.ttf', 60)
-def draw_background():
+def background():
     for y in range(0, height, cell):
         for x in range(0, width, cell):
             color = green1 if (x//cell + y//cell) % 2 == 0 else green2
             pygame.draw.rect(screen, color, (x, y, cell, cell))
 
-def spawn_food():
+def foodran():
     while True:
         fx = random.randrange(0, width, cell)
         fy = random.randrange(0, height, cell)
@@ -42,7 +43,7 @@ def spawn_food():
         if all(not fr.colliderect(r) for r in snake):
             return fr
 
-food = spawn_food()
+food = foodran()
 
 running = True
 while running:
@@ -67,39 +68,43 @@ while running:
     head.y += direction[1]
 
     if head.x < 0 or head.x >= width or head.y < 0 or head.y >= height:
+        diesound.play()
         running = False
 
     for r in snake:
         if head.colliderect(r):
+            diesound.play()
             running = False
 
     snake.insert(0, head)
 
     if head.colliderect(food):
         score += 1
+        eatsound.play()
+
         if score % 4 == 0:
             level += 1
             speed += 2
-        food = spawn_food()
+        food = foodran()
     else:
         snake.pop()
 
-    draw_background()
+    background()
 
     for r in snake:
         pygame.draw.rect(screen, blue, r)
 
-    screen.blit(food_img, food)
+    screen.blit(foodimg, food)
     text = font.render(f"Score: {score}   Level: {level}", True, (0,0,0))
     screen.blit(text, (8,8))
     pygame.display.update()
 
-    
-screen.fill((150, 230, 150))  
+else:    
+   screen.fill((150, 230, 150))  
 
-gotext = gameoverfont.render("GAME OVER", True, 'black')  
-leveltext = font.render(f"LEVEL: {level}", True, 'black')
-scoretext = font.render(f"SCORE: {score}", True, 'black')
+   gotext = gameoverfont.render("GAME OVER", True, 'black')  
+   leveltext = font.render(f"LEVEL: {level}", True, 'black')
+   scoretext = font.render(f"SCORE: {score}", True, 'black')
 
 screen.blit(gotext, (120, 200))
 screen.blit(leveltext, (210, 300))
